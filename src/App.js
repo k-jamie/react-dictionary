f2cc077d27f1038e8af16486d3e9644d41f0a7d0
k-jamie <79115673+k-jamie@users.noreply.github.com>
2021-12-11 23:13:21 +0000

    initial commit - project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header className="App-header"> </header>
+      
+ <h1>dictionary.</h1>
+     
+    </div>
+  );
+}
+
+export default App;
