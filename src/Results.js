f1df565b8f985b9875e587d82c681e9ef2c02fd1
k-jamie <79115673+k-jamie@users.noreply.github.com>
2021-12-12 15:48:48 +0000

    updated phonetics file to only have first audio file

--- src/Results.js
+++ src/Results.js
@@ -5,14 +5,9 @@ import Phonetic from "./Phonetic";
 export default function Results(props) {
     if (props.results) {
         return <div className="Results">
-            <h2>{props.results.word}</h2>
-            {props.results.phonetics.map(function (phonetic, index) {
-               return (
-        <div key={index}>
-                <Phonetic phonetic={phonetic} />
-        </div>
-               ); 
-            })}
+           <h2>{props.results.word}</h2>
+            <Phonetic phonetic={props.results.phonetics[0]} />
+            <br />
             {props.results.meanings.map(function (meaning, index) {
                 return (
                     <div key={index}>
