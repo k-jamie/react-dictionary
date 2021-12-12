2ee2d7bf2e347c9ffc6fb30dc6680405c34cc1b2
k-jamie <79115673+k-jamie@users.noreply.github.com>
2021-12-12 01:25:02 +0000

    created new state called results, created two new components to handle the results and parse the data

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import './Dictionary.css';
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
     function handleResponse(response) {
-        console.log(response.data[0]);
+        setResults(response.data[0]);
     }
 
     function search(event) {
@@ -26,7 +28,8 @@ return (
             <form onSubmit={search}>
             <input type="search"
                 onChange={handleKeywordChange} />
-            </form>
+        </form>
+        <Results results={results} />
         </div>
     );
     
