f7dc9f5ddfcdba23177fa5763287c5763e4e6931
k-jamie <79115673+k-jamie@users.noreply.github.com>
2021-12-12 00:04:46 +0000

    set up dictionary component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
-    <div className="container">
-      <header className="App-header"> </header>
-      
- <h1>dictionary.</h1>
+    <div className="Container">
+      <header className="App-header"> <h1>dictionary.</h1></header>
+      <main className="App-main">
+        <Dictionary />
+      </main>
+ <footer className="App-footer">Coded by Karen Jamieson, open-sourced on GitHub, hosted on Netlify.</footer>
      
     </div>
   );
