4ef6cc505954c868421c313996d462c70cef88c8
k-jamie <79115673+k-jamie@users.noreply.github.com>
2021-12-18 18:54:39 +0000

    updated default word

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="Container">
       <header className="App-header"> <h1>dictionary.</h1></header>
       <main className="App-main">
-        <Dictionary defaultKeyword="hello" />
+        <Dictionary defaultKeyword="amazing" />
       </main>
  <footer className="App-footer">Coded by <a href="https://www.linkedin.com/in/karen-jamieson-5b464a103/" target="_blank" rel="noreferrer">Karen Jamieson</a>, open-sourced on <a href="https://github.com/k-jamie/react-dictionary" target="_blank" rel="noreferrer">GitHub</a>, hosted on <a href="https://pedantic-shaw-f5b989.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>.</footer>
      
